3b794e04631d189e466ea95d85d6286f1174aecd
Kyle Taylor <kyle.a.taylor@gmail.com>
2020-04-26 10:36:52 -0600

    house keeping

--- rio_reclassify/reclassify.py
+++ rio_reclassify/reclassify.py
@@ -53,57 +53,5 @@ def reclassify(src_image, table, field="di", dst_image=None):
     else:
         return results
 
-#
-# MAIN
-#
-
-src_filename = "gssurgo_2020_mukey_30m.tif"
-
-src = rio.open(src_filename)
-
-src_params = {}
-src_params['width'] = src.width
-src_params['height'] = src.height
-src_params['dtype'] = src.dtypes[0]
-
-del src
-
-di_pi_table = pd.read_csv("di_pi_table.csv")[["mukey", "Final_DI", "Final_PI"]]
-di_pi_table = di_pi_table.rename(
-    columns={"mukey": "mukey", "Final_DI": "di", "Final_PI": "pi"}
-)
-
-pool = multiprocessing.Pool(4)
-windows = [w[1] for w in list(rio.open(src_filename).block_windows(1))]
-
-results = list(pool.starmap(
-    reclassify_window, [ (src_filename, w, di_pi_table, "di") for w in windows ]
-))
-
-with rio.open(
-    "di_2020.tif",
-    "w",
-    driver="GTiff",
-    width=src_params['width'],
-    height=src_params['height'],
-    count=1,
-    dtype=src_params['dtype'],
-) as dst:
-    for i in range(results):
-        dst.write(results[i], window=windows[i], indexes=1)
-
-results = list(pool.starmap(
-    reclassify_window, [ (src_filename, w, di_pi_table, "pi") for w in windows ]
-))
-
-with rio.open(
-    "pi_2020.tif",
-    "w",
-    driver="GTiff",
-    width=src_params['width'],
-    height=src_params['height'],
-    count=1,
-    dtype=src_params['dtype'],
-) as dst:
-    for i in range(results):
-        dst.write(results[i], window=windows[i], indexes=1)
+def reclassify_worker:
+    pass
